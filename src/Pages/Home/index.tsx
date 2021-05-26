6f6d2b18dd026208cd387756e13577d29c1d5f3d
MaxSapeyko <45255586+MaxSapeyko@users.noreply.github.com>
2021-05-26 17:43:19 +0300

    added offer to main-page (#12)
    added offer to main-page (#12)

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,9 +7,19 @@ const Home: FC = () => {
 
   return (
     <div className={classes.root}>
-      <p>Home</p>
+      <section className='main'>
+        <div className='offer_wrapper'>
+          <h1 className='offer_title'>Car Trade</h1>
+          <p className='offer_text'>
+            На сайте находится таможенный калькулятор по действующим тарифам (Закон 8487/8488) на растаможку. С помощью этого калькулятора Вы можете сравнить стоимость растаможивания автомобилей с ДВС, элетромобиль и гибридов.
+          </p>
+        </div>
+        <div className='img_wrapper'>
+          <img src='/assets/img/bmw.png' alt='' />
+        </div>
+      </section>
     </div>
-  )
+  );
 };
 
 export default Home;
