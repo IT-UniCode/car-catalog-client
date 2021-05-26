1776b9a083c125eb8a256634af763dc875ab4d4d
MaxSapeyko <maks.sapeyko@gmail.com>
2021-05-26 16:29:44 +0300

    fixed menu item bg after routing

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -10,28 +10,26 @@ const { Header } = Layout;
 const CustomHeader: FC = () => {
   const classes = useStyles();
   const history = useHistory();
-  const [path, setPath] = useState(['/']);
-
-  useEffect(() => {
-    console.log(history.location.pathname);
-    
-    setPath([`${history.location.pathname}`]);
-  }, [history.location.pathname]);
+  const [path, setPath] = useState([`${history.location.pathname}`]);
 
   return (
     <Header className={classes.root}>
       <div className='container'>
         <Menu theme='dark' mode='horizontal' selectedKeys={path}>
           <Menu.Item key='/'>
-            <Link to='/' className='menu_link'>
+            <Link to='/' onClick={() => setPath(['/'])} className='menu_link'>
               Главная
             </Link>
           </Menu.Item>
           <Menu.Item key='/catalog'>
-            <Link to='/catalog'>Каталог</Link>
+            <Link to='/catalog' onClick={() => setPath(['/catalog'])}>
+              Каталог
+            </Link>
           </Menu.Item>
           <Menu.Item key='/calc'>
-            <Link to='/calc'>Калькулятор</Link>
+            <Link to='/calc' onClick={() => setPath(['/calc'])}>
+              Калькулятор
+            </Link>
           </Menu.Item>
         </Menu>
       </div>
