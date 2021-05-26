afca34099815e4054eaf8c05e456dafe45a81079
MaxSapeyko <maks.sapeyko@gmail.com>
2021-05-26 17:42:31 +0300

    added offer to main-page

--- src/Pages/Home/style.ts
+++ src/Pages/Home/style.ts
@@ -2,8 +2,34 @@ import { createUseStyles } from 'react-jss';
 
 const useStyles = createUseStyles({
   root: {
-    
-  }
-})
+    '& .main': {
+      display: 'flex',
+      justifyContent: 'space-between',
+
+      '& .offer_wrapper': {
+        maxWidth: '400px',
+        width: '100%',
+
+        '& .offer_title': {
+          marginTop: '60px',
+          fontSize: '36px',
+        },
+        '& .offer_text': {
+          fontSize: '18px',
+        },
+      },
+
+      '& .img_wrapper': {
+        maxWidth: '700px',
+        width: '100%',
+
+        '& img': {
+          paddingTop: '60px',
+          width: '100%',
+        },
+      },
+    },
+  },
+});
 
 export default useStyles;
